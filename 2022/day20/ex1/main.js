2e1f4a724404a7e1e7d9a382e1c60485ce0bfa38
itsRed-v2 <paul.charignon.v2@gmail.com>
2022-12-21 19:14:57 +0100

    2022 day 20

---
+++ 2022/day20/ex1/main.js
@@ -0,0 +1,36 @@
+import { input } from "../input.js";
+
+const nbList = input.split("\n").map(line => {
+	return { value: parseInt(line) }
+});
+
+const mixingList = [...nbList];
+
+for (const refNb of nbList) {
+	let index = mixingList.indexOf(refNb);
+	mixingList.splice(index, 1);
+
+	let newIndex = getNewIndex(index, refNb.value, nbList.length);
+	mixingList.splice(newIndex, 0, refNb);
+}
+
+const zeroRef = nbList.find(o => o.value === 0);
+const zeroIndex = mixingList.indexOf(zeroRef);
+
+const valAt1k = mixingList[getIndexAfter(zeroIndex, 1000, mixingList.length)];
+const valAt2k = mixingList[getIndexAfter(zeroIndex, 2000, mixingList.length)];
+const valAt3k = mixingList[getIndexAfter(zeroIndex, 3000, mixingList.length)];
+console.log(valAt1k.value + valAt2k.value + valAt3k.value);
+
+function getNewIndex(oldIndex, offset, arrayLength) {
+	let newIndex = oldIndex + offset
+	while (newIndex >= arrayLength) newIndex -= arrayLength - 1;
+	while (newIndex <= 0) newIndex += arrayLength - 1;
+	return newIndex;
+}
+
+function getIndexAfter(oldIndex, offset, arrayLength) {
+	let newIndex = oldIndex + offset;
+	newIndex %= arrayLength;
+	return newIndex;
+}
